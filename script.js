081e983162accfd673bf017b409dd6e9c9616fcb
Krista Boka <bokakrista06@gmail.com>
2024-07-24 14:32:44 +0000

    undo the script

--- script.js
+++ script.js
@@ -126,25 +126,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
   });
   
-  const path = require('path');
 
-module.exports = {
-  entry: './src/script.js', // Change entry point to script.js
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devServer: {
-    contentBase: './dist'
-  }
-};
+  
 
 
