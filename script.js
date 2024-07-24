13c325680a7faf563936d88bef1caa379912a6aa
Krista Boka <bokakrista06@gmail.com>
2024-07-24 14:17:35 +0000

    Add modules

--- script.js
+++ script.js
@@ -126,3 +126,25 @@ document.addEventListener("DOMContentLoaded", function() {
   
   });
   
+  const path = require('path');
+
+module.exports = {
+  entry: './src/script.js', // Change entry point to script.js
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
+
+
